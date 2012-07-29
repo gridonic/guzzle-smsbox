58f1478fc4b5c65b9950b4a72d576e4490b38b3d
Peter Siska <pesche@gridonic.ch>
2012-07-29 18:14:55 +0200

    Reconfigured phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,18 +1,16 @@
 <?php
-
-error_reporting(E_ALL | E_STRICT);
-
 // Ensure that composer has installed all dependencies
-if (!file_exists(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'composer.lock')) {
+if ( ! file_exists(__DIR__.'/../composer.lock')) {
     die("Dependencies must be installed using composer:\n\ncomposer.phar install --dev\n\n"
         . "See https://github.com/composer/composer/blob/master/README.md for help with installing composer\n");
 }
 
 // Include the composer autoloader
-$loader = require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
+$loader->add('Gridonic\Guzzle\SmsBox\Tests', __DIR__);
 
 // Register services with the GuzzleTestCase
-Guzzle\Tests\GuzzleTestCase::setMockBasePath(__DIR__ . DIRECTORY_SEPARATOR . 'mock');
+Guzzle\Tests\GuzzleTestCase::setMockBasePath(__DIR__.'/mock');
 
 Guzzle\Tests\GuzzleTestCase::setServiceBuilder(\Guzzle\Service\Builder\ServiceBuilder::factory(array(
     'test.smsbox' => array(
